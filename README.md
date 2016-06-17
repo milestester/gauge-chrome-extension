48348fb2c70a22db1097245f502d61f48e678fbb
Miles Tester <milesdeant@gmail.com>
2016-06-17 11:53:55 -0700

    update website deletion, update readme, basic URL validation

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A very simple chrome extension that tracks and displays time spent on certain we
 
 To install on your machine, clone repository or download and unzip.  Under 'chrome://extensions/' select 'Load unpacked extension...' and navigate to where you saved the 'Gauge' extension files.
 
+Icons made by [http://www.flaticon.com/authors/madebyoliver] from www.flaticon.com
--- options/options.js
+++ options/options.js
@@ -14,7 +14,7 @@ function buildOptionLabel(labelText, colour) {
   var changeColourSpan = createNodeWithText("span", "colour");
   var dividerSpan = createNodeWithText("span", "|");
   deleteSpan.className = "options delete";
-  changeColourSpan.className = "options";
+  changeColourSpan.className = "options colour";
   dividerSpan.className = "options";
 
   node.appendChild(deleteSpan);
@@ -24,7 +24,7 @@ function buildOptionLabel(labelText, colour) {
   document.getElementById("current-websites").appendChild(node);
 
   setColourSpanEventListener(changeColourSpan, colour, labelText, node);
-  setDeleteSpanEventListener(deleteSpan, node, colour);
+  deleteSpan.onmousedown = deleteItem(node);
 }
 
 function setColourSpanEventListener(changeColourSpan, colour, labelText, node) {
@@ -37,16 +37,6 @@ function setColourSpanEventListener(changeColourSpan, colour, labelText, node) {
         node.style.backgroundColor = colour;
       }
     });
-  };
-}
-
-function setDeleteSpanEventListener(deleteSpan, node, colour) {
-  deleteSpan.onmousedown = deleteItem(node);
-  deleteSpan.onmouseover = function(e) {
-    node.style.backgroundColor = "#e74c3c";
-  };
-  deleteSpan.onmouseout = function(e) {
-    node.style.backgroundColor = colour;
   };
 }
 
@@ -77,31 +67,40 @@ function setEventListeners() {
     var keyCode = e.keyCode || e.which;
     if (keyCode == '13'){
       var newURL = this.value;
-      var domain = TimeTracker.getDomainFromHostName(newURL);
-      var that = this;
-      LocalStorageManager.getSingleKey(domain, function(siteObj) {
-        if(siteObj == null) {
-          // Site not currently being tracked
-          var colour = document.getElementById("websiteInput").parentNode.style.backgroundColor;
-          var site = new Site(domain, undefined, {}, colour);
-          site.saveToLocalStorage();
-          buildOptionLabel(domain, colour);
-        }
-        that.value = "";
-        var colour = randomColour(0.7);
-        document.getElementById("websiteInput").parentNode.style.backgroundColor = colour;
-        document.getElementById("websiteInput").style.backgroundColor = colour;
-      });
+      if(validURL(newURL)) {
+        var domain = TimeTracker.getDomainFromHostName(newURL);
+        var that = this;
+        LocalStorageManager.getSingleKey(domain, function(siteObj) {
+          if(siteObj == null) {
+            // Site not currently being tracked
+            var colour = document.getElementById("websiteInput").parentNode.style.backgroundColor;
+            var site = new Site(domain, undefined, {}, colour);
+            site.saveToLocalStorage();
+            buildOptionLabel(domain, colour);
+          }
+          that.value = "";
+          var colour = randomColour(0.7);
+          that.style.borderColor = "#fff";
+          that.parentNode.style.backgroundColor = colour;
+          that.style.backgroundColor = colour;
+        });
+      } else {
+        this.style.borderColor = "red";
+      }
     }
   }
 }
 
-// Bug, sometimes chart not showing up because of random colour?
-var randomColourFactor = function() {
+function validURL(newURL) {
+  var result = /^[^www\.](.)+(\.(.)+)+/.test(newURL);
+  return result;
+}
+
+function randomColourFactor() {
   return Math.round(Math.random() * 255);
 };
 
-var randomColour = function(opacity) {
+function randomColour(opacity) {
   return 'rgba(' + randomColourFactor() + ',' + randomColourFactor() + ',' + randomColourFactor() + ',' + (opacity || '.3') + ')';
 };
 
